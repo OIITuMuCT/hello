42ab57056494e152fa1a4769b6091dde698d43af
OIITuMuCT <vataman@live.ru>
2024-11-05 15:56:57 +0300

    all forms fields

--- firstapp/forms.py
+++ firstapp/forms.py
@@ -12,4 +12,16 @@ class UserForm(forms.Form):
     email = forms.EmailField(label="Электронный адрес",help_text="Обязательный символ - @")
     file = forms.FileField(label="Файл")
     file_path = forms.FilePathField(label="Выберите файл", path="/home/neko/Documents", allow_files="True", allow_folders="True")
-    
+    num = forms.FloatField(label="Введите число")
+    ip_adres = forms.GenericIPAddressField(label=" Enter Ip address", help_text="Example 127.0.0.1")
+    num_json = forms.JSONField(label="Данные формата JSON")
+    country = forms.MultipleChoiceField(label="Выберите страны", choices=((1, "Aнглия"), (2, "Германия"), (3, "Испания"), (4, "Россия")))
+    city = forms.TypedChoiceField(label="Choice city", empty_value=None, choices=((1, "Moscow"), (2, "London"), (3, "Paris")))
+    city_multiple = forms.TypedMultipleChoiceField(label="Выберите город", empty_value=None, choices=((1, "Москва"),
+                                                                                                      (2, "Воронеж"),
+                                                                                                      (3, "Курск"),
+                                                                                                      (4, "Томск")))
+    combo_text = forms.ComboField(label="Введите данные комбофилд",fields=[forms.CharField(max_length=20), forms.EmailField()])
+    combo_multi_text = forms.MultiValueField(label="комплескное поле", fields=(
+        forms.CharField(max_length=20), forms.EmailField()
+    ))
